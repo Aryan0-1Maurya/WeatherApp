b9e157e1ff1eb4101b4c3595a76271a2e919e2ce
Aryan Maurya <136960772+Aryan0-1Maurya@users.noreply.github.com>
2024-01-04 16:49:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#6b7b8c",
+        "weather-secondary": "#3b4d61",
+        
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
+  },
+  plugins: [],
+};
